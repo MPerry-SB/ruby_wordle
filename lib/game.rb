8aaf2562ef7bc608a7eb9a6f6f636828aff464b7
Mitchell Perry <mitchell.perry@simplybusiness.co.uk>
2023-05-22 18:50:59 +0100

    refactor: init a game objec with default value of attempts set to 0

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,15 @@
 class Game
+  def initialize()
+    @attempts = 0
+  end
+
+  attr_reader :attempts
 
   def word(input)
     input
   end
 
-  def attempts
-    0
-  end
+  # def attempts
+  #   0
+  # end
 end
