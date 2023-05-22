6ee4f3a08eeb6741ea68c8a979b000851c337c1a
Mitchell Perry <mitchell.perry@simplybusiness.co.uk>
2023-05-22 19:32:15 +0100

    remove Guess class

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,6 +1,4 @@
 require './lib/game'
-require './lib/guess'
-
 
 describe Game do
   describe '#new' do
@@ -30,58 +28,56 @@ describe Game do
   end
 end
 
-describe Guess do
-  describe 'guessing a word' do
-    it 'allows a user to input a guess' do
-    end
-    it 'stores the current guess' do
-    end
-    it 'stores another guess' do
-    end
-    it 'will only allow the user to enter 5 letter words' do
-    end
+describe 'guessing a word' do
+  it 'allows a user to input a guess' do
+  end
+  it 'stores the current guess' do
+  end
+  it 'stores another guess' do
+  end
+  it 'will only allow the user to enter 5 letter words' do
   end
 end
 
-# describe 'checking for a correct guess' do
-#   it 'checks if the guess is correct' do
-#   end
-#   it 'updates the current game as WON' do
-#   end
-# end
+describe 'checking for a correct guess' do
+  it 'checks if the guess is correct' do
+  end
+  it 'updates the current game as WON' do
+  end
+end
 
-# describe 'checking for incorrect guess' do
-#   it 'checks if the guess is incorrect' do
-#   end
-#   it 'updates the number of attemps' do
-#   end
-# end
+describe 'checking for incorrect guess' do
+  it 'checks if the guess is incorrect' do
+  end
+  it 'updates the number of attemps' do
+  end
+end
 
-# describe 'check letter position' do
-#   it 'identifies the correctly positioned letters of a guess' do
-#   end
-# end
+describe 'check letter position' do
+  it 'identifies the correctly positioned letters of a guess' do
+  end
+end
 
-# describe 'check letter existence' do
-#   it 'identifies if a letter is within a guess' do
-#   end
-# end
+describe 'check letter existence' do
+  it 'identifies if a letter is within a guess' do
+  end
+end
 
-# describe 'winning the game' do
-#   it 'can identify when a player has won the game' do
-#   end
-# end
+describe 'winning the game' do
+  it 'can identify when a player has won the game' do
+  end
+end
 
-# describe 'losing the game' do
-#   it 'can identify when a player has lost the game' do
-#   end
-#   it 'will not allow the user to have more than 6 attempts' do
-#   end
-# end
+describe 'losing the game' do
+  it 'can identify when a player has lost the game' do
+  end
+  it 'will not allow the user to have more than 6 attempts' do
+  end
+end
 
-# describe 'game reset' do
-#   it 'resets the game after winning' do
-#   end
-#   it 'resets the game after losing' do
-#   end
-# end
+describe 'game reset' do
+  it 'resets the game after winning' do
+  end
+  it 'resets the game after losing' do
+  end
+end
