4bbeeaaab46d6a8c083e7e740f2e03a03098ee6e
Mitchell Perry <mitchell.perry@simplybusiness.co.uk>
2023-05-23 23:08:47 +0100

    refactor test and Game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,5 @@
 class Game
-  def initialize()
+  def initialize
     @attempts = 0
     @word = create_word
     @user_guesses = []
@@ -14,11 +14,11 @@ class Game
 
   def check_user_guess
     last_guess = @user_guesses.last
-    return "Please enter a word with exactly 5 letters" if last_guess.chars.count != 5
-    return @status = 'GAME WON' && "Correct guess! You Win!" if last_guess == @word 
+    return 'Please enter a word with exactly 5 letters' if last_guess.chars.count != 5
+    return @status = 'GAME WON' && 'Correct guess! You Win!' if last_guess == @word
   end
 
   def create_word
-    ["HELLO", "SHELL", "FOOBA"].sample
+    %w[HELLO SHELL FOOBA].sample
   end
 end
