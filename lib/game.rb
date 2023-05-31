19dcded6b486e82d024f3d168408b1ac59bc7b90
Mitchell Perry <mitchell.perry@simplybusiness.co.uk>
2023-05-31 21:34:53 +0100

    refactor extract user_guess logic into seperate methods

--- lib/game.rb
+++ lib/game.rb
@@ -11,12 +11,24 @@ class Game
   def user_guess(string)
     return 'Please enter a word with exactly 5 letters' if string.chars.count != 5
 
-    @attempts += 1
-    @user_guesses << string
+    update_attempts
+    add_user_guess(string)
     check_user_guess(string)
   end
 
   private
+
+  def create_word
+    %w[HELLO SHELL FOOBA].sample
+  end
+
+  def update_attempts
+    @attempts += 1
+  end
+
+  def add_user_guess(string)
+    @user_guesses << string
+  end
 
   def check_user_guess(string)
     user_guess = string.strip
@@ -26,9 +38,5 @@ class Game
       return 'Correct guess! You Win!'
     end
     'Incorrect guess! try again....'
-  end
-
-  def create_word
-    %w[HELLO SHELL FOOBA].sample
   end
 end
